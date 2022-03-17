7f5216a00206f899d17906461033ac7b4a2adc4b
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-03-18 12:48:38 +1300

    Some UI

--- src/main/com/snake/ui/SnakeUI.java
+++ src/main/com/snake/ui/SnakeUI.java
@@ -1,7 +1,54 @@
 package com.snake.ui;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class SnakeUI {
+
+    private boolean inMenu = true;
+
+    final private Toolkit awtToolkit = Toolkit.getDefaultToolkit();
+
+    private JFrame frame;
+    private JPanel menu;
+    private JPanel game;
+
     public SnakeUI() {
 
+        // Create window
+        frame = new JFrame();
+
+        Dimension screenSize = awtToolkit.getScreenSize();
+        Point screenCenter = new Point(screenSize.width / 2, screenSize.height / 2);
+        int frameSize = (int) (Math.min(screenSize.height, screenSize.width) * 0.65);
+
+        frame.setBounds(
+                screenCenter.x - (frameSize / 2), screenCenter.y - (frameSize / 2),
+                frameSize, frameSize
+        );
+        frame.setTitle("Snake");
+        frame.setIconImage(null);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        // Create menu panel
+        menu = new JPanel();
+        menu.setBackground(Color.BLUE);
+        menu.setSize(frameSize, frameSize);
+        menu.setVisible(inMenu);
+        frame.add(menu);
+
+        // Create game panel
+        game = new JPanel();
+        game.setBackground(Color.RED);
+        game.setSize(frameSize, frameSize);
+        game.setVisible(!inMenu);
+        frame.add(game);
+    }
+
+    private void resize() {
+        Dimension frameSize = frame.getSize();
+        menu.setSize(frameSize);
+        game.setSize(frameSize);
     }
 }
